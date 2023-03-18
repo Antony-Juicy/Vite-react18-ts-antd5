136a0aae75a6a7693e823ad084530b867cb48ac7
Antony-Juicy <71221521+Antony-Juicy@users.noreply.github.com>
2023-03-18 21:56:00 +0800

    fix:"单位换算"

--- src/views/index/index.tsx
+++ src/views/index/index.tsx
@@ -1,83 +1,84 @@
-import React, { useState, useEffect, FC } from 'react'
-import cx from 'classnames'
-import Style from './index.module.less'
+import React, { useState } from 'react'
 
-let oneSecInterval: NodeJS.Timeout
-let thirtySecInterval: NodeJS.Timeout
+type Units = {
+  [key: string]: number
+}
 
-const TrafficLightController: FC = () => {
-  const [isRedLightOpened, setIsRedLightOpened] = useState<boolean>(false)
-  const [isYellowLightOpened, setIsYellowLightOpened] = useState<boolean>(false)
-  const [isGreenLightOpened, setIsGreenLightOpened] = useState<boolean>(false)
+const storageUnits: Units = {
+  B: 1,
+  KB: 1024,
+  MB: 1048576,
+  GB: 1073741824,
+  TB: 1099511627776,
+}
 
-  const circleLights = () => {
-    let sec: number = 0
-    const cicle = () => {
-      setIsRedLightOpened(true)
+const StorageConverter = () => {
+  const [inputValue, setInputValue] = useState<number>(0)
+  const [unit, setUnit] = useState<string>('B')
 
-      oneSecInterval = setInterval(() => {
-        sec = sec + 1
-        console.log('@!sec', sec)
-
-        if (sec === 20) {
-          setIsRedLightOpened(false)
-          setIsYellowLightOpened(true)
-        } else if (sec === 25) {
-          setIsYellowLightOpened(false)
-          setIsGreenLightOpened(true)
-        } else if (sec > 25) {
-          if (sec >= 30) {
-            setIsGreenLightOpened(false)
-
-            sec = 0
-            clearInterval(oneSecInterval)
-          } else if (!(sec % 2)) {
-            setIsGreenLightOpened(false)
-            console.log('====')
-          } else if (sec % 2) {
-            setIsGreenLightOpened(true)
-            console.log('----')
-          }
-        }
-      }, 1000)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value.trim()
+    if (/^\d+$/.test(input)) {
+      setInputValue(Number(input))
     }
-    cicle()
-    thirtySecInterval = setInterval(() => {
-      cicle()
-    }, 30 * 1000)
   }
 
-  useEffect(() => {
-    circleLights()
-    return () => {
-      clearInterval(thirtySecInterval)
-      clearInterval(oneSecInterval)
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setUnit(e.target.value)
+  }
+
+  /**
+   * @date  2023-03-14
+   * @value   value
+   * @from     单位
+   * @to      转换的单位
+   */
+  const convert = (value: number, from: keyof Units, to: keyof Units) =>
+    (value * storageUnits[from]) / storageUnits[to]
+
+  let convertedValue = convert(inputValue, unit, 'B')
+  console.log(convertedValue, 'convertedValue')
+  let displayUnit = unit
+
+  if (convertedValue >= storageUnits.TB) {
+    convertedValue /= storageUnits.TB
+    displayUnit = 'TB'
+  } else if (convertedValue >= storageUnits.GB) {
+    convertedValue /= storageUnits.GB
+    displayUnit = 'GB'
+  } else if (convertedValue >= storageUnits.MB) {
+    convertedValue /= storageUnits.MB
+    displayUnit = 'MB'
+  } else if (convertedValue >= storageUnits.KB) {
+    convertedValue /= storageUnits.KB
+    displayUnit = 'KB'
+  }
+
+  function fun(n: any, o: any) {
+    console.log(o, '0000===')
+    return {
+      fun: function (m: any) {
+        return fun(m, n)
+      },
     }
-  }, [])
+  }
+  let c = fun(0).fun(1)
+  c.fun(2)
+  c.fun(3)
 
   return (
-    <>
-      <div>
-        <div
-          className={cx(
-            Style.defaultLight,
-            isRedLightOpened ? Style.redLight : Style.greyLight
-          )}
-        />
-        <div
-          className={cx(
-            Style.defaultLight,
-            isYellowLightOpened ? Style.yellowLight : Style.greyLight
-          )}
-        />
-        <div
-          className={cx(
-            Style.defaultLight,
-            isGreenLightOpened ? Style.greenLight : Style.greyLight
-          )}
-        />
-      </div>
-    </>
+    <div>
+      <input type="text" onChange={handleChange} />
+      <select value={unit} onChange={handleSelectChange}>
+        {Object.keys(storageUnits).map((unit) => (
+          <option key={unit} value={unit}>
+            {unit}
+          </option>
+        ))}
+      </select>
+      <div>{`${convertedValue.toFixed(2)} ${displayUnit}`}</div>
+    </div>
   )
 }
-export default TrafficLightController
+
+export default StorageConverter
